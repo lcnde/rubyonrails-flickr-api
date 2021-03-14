023e5b988f85f25379585a18891f0227548c34fc
lcnde <176673026+lcnde@users.noreply.github.com>
2021-03-14 11:01:57 +0100

    works

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,23 @@
 class StaticPagesController < ApplicationController
   require 'flickr'
-  @@flickr = Flickr.new ENV['key'], ENV['secret']
+  #@@flickr = Flickr.new ENV['key'], ENV['secret']
 
   def index
+    flickr = Flickr.new ENV['key'], ENV['secret']
     if params[:user] != nil && params[:user] != ''
-      @images = @@flickr.photos.search(user_id: params[:user])
+      #if Flickr::FailedResponse  #flickr.people.getPhotos(user_id: params[:user])
+       # @images = flickr.people.getPhotos(user_id: params[:user])
+      #else
+       # @images = nil
+      #end
+      begin 
+        @images = flickr.people.getPhotos(user_id: params[:user])
+      rescue Flickr::FailedResponse
+        flash.now[:alert] = 'User not found or the user has no photos.'
+        #render :index
+      end
+    else
+      render :index
     end
   end
 
